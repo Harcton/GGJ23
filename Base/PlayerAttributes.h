aad557dd08a2bc4d972153a56fe217c513c7145c
Juuso Turunen <juusoturunen94@hotmail.com>
2023-02-05 02:26:04 +0200

    Base: Increase default fire rate

--- Base/PlayerAttributes.h
+++ Base/PlayerAttributes.h
@@ -8,7 +8,7 @@ struct PlayerAttributes
 	float movementSpeed = 30.0f;
 	float weaponDamage = 10.0f;
 	float weaponRange = 100.0f;
-	float weaponRate = 1.0f;
+	float weaponRate = 1.5f;
 	float weaponVelocity = 100.0f;
 	float weaponSpread = 0.0f; // Radians, used only if shot size is greater than 1
 	uint8_t weaponShotSize = 1;
